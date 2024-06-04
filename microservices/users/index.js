8b5bc959311a7da2ecef17ce0d6fdc4ffd6b34f5
Seba <sebastiantorresola3@gmail.com>
2024-06-03 18:41:15 -0700

    CRUD endpoints

--- microservices/users/index.js
+++ microservices/users/index.js
@@ -2,10 +2,45 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const User = require("./models/user");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = 3000;
 cors();
 
-app.listen(3000, () => {
+// TODO: Input validation for all endpoints
+app.get("/:id", async (req, res) => {
+  const response = await new User((id = req.params.id)).getUserById(
+    req.params.id
+  );
+  res.send(response);
+});
+
+app.post("/", async (req, res) => {
+  // TODO: Salt + Hash password
+  const response = await new User().createUser(
+    req.body.email,
+    req.body.password
+  );
+  res.send(response);
+});
+
+app.put("/", async (req, res) => {
+  const response = await new User().updateUser(
+    req.body.id,
+    req.body.email,
+    req.body.password
+  );
+  res.send(response);
+});
+
+app.delete("/:id", async (req, res) => {
+  const response = await new User().deleteUser(req.params.id);
+  res.send(response);
+});
+
+app.listen(PORT, () => {
   console.log("App is now running at port");
 });
