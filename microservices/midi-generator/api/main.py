f685459f1c49b93e4c93a631ddb0d8645d6ff9fb
Seba <sebastiantorresola3@gmail.com>
2024-07-30 22:49:18 -0700

    Midi-gen: Create FastAPI route to expose midi-gen service

---
+++ microservices/midi-generator/api/main.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import FileResponse
+import os
+
+from model.transcriptions import generateTranscription
+
+app = FastAPI()
+
+@app.post("/transcriptions")
+async def createTranscription(file: UploadFile = File(...)):
+    fileName = file.filename.split('.')[0]
+    audioPath = os.path.join(os.path.dirname(__file__), '../files/inputs/', file.filename)
+    with open(audioPath, 'wb') as f:
+        f.write(file.file.read())
+
+    outputPath = os.path.join(os.path.dirname(__file__), '../files/outputs/')
+
+    generateTranscription(audioPath, outputPath, fileName)
+
+    return FileResponse(path=outputPath+fileName+'.mid', media_type='audio/midi')
+
+
