9d8a41f30f6b1bd5d9d3804118ce9e0c4321eb22
Seba <sebastiantorresola3@gmail.com>
2024-10-17 00:05:04 -0700

    Midi-gen: Beam cloud GPU integration

--- services/midi-generator/api/main.py
+++ services/midi-generator/api/main.py
@@ -1,30 +1,74 @@
-from typing import Annotated
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import FileResponse
 import os
+import asyncio
+from google.protobuf.json_format import MessageToDict
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, UploadFile, File, Request, HTTPException
+from fastapi.responses import FileResponse
+from note_seq.midi_io import midi_file_to_note_sequence
+from piano_transcription_inference.utilities import write_events_to_midi
 
-from model.transcriptions import generateTranscription
+from model.transcriptions import getMidiEvents
 from .db import getDbPool
 
 @asynccontextmanager
 async def lifespan(a):
    pool = await getDbPool()
-   yield {"pool": pool}
+   yield { 
+            "pool": pool,
+            "count": 1
+        } # passes pool into request.state
    await pool.close()
  
 app = FastAPI(lifespan=lifespan)
 
 @app.post("/transcriptions")
-async def createTranscription(file: UploadFile = File(...)):
-    fileName = file.filename.split('.')[0]
-    audioPath = os.path.join(os.path.dirname(__file__), '../files/inputs/', file.filename)
-    with open(audioPath, 'wb') as f:
-        f.write(file.file.read())
+async def createTranscription(request: Request, file: UploadFile = File(...)):
+    print(file.filename)
+    if file.size > 20000000: # 20mb 
+        raise HTTPException(400, "File size too large")
 
-    outputPath = os.path.join(os.path.dirname(__file__), '../files/outputs/')
+    # Get filename without extension
+    fileName = list(file.filename)
+    idx = len(fileName) - 1
+    while fileName[idx] != ".":
+        fileName.pop()
+        idx -= 1
+    fileName.pop() # remove last .
+    fileName = "".join(fileName)
 
-    generateTranscription(audioPath, outputPath, fileName)
+    # Generate transcription
+    outputFolderPath = os.path.join(os.path.dirname(__file__), '../files/outputs/')
+    est_note_events, est_pedal_events = await getMidiEvents(file)
 
-    return FileResponse(path=outputPath+fileName+'.mid', media_type='audio/midi')
+    # Write MIDI events to file
+    midiFilePath = outputFolderPath+fileName+'.mid'
+    write_events_to_midi(start_time=0, note_events=est_note_events, 
+        pedal_events=est_pedal_events, midi_path=midiFilePath)
+    print('Write out to {}'.format(midiFilePath))
+
+    noteSequenceProto = midi_file_to_note_sequence(midiFilePath)
+    noteSequence = MessageToDict(noteSequenceProto)
+    
+    # Insert transcription and notes into DB
+    async with request.state.pool.connection() as conn:
+        async with conn.cursor() as curs:
+            insertTranscriptionSql = """
+                                INSERT INTO transcriptions (qpm, song_name) VALUES (%s, %s) 
+                                RETURNING transcription_id;
+                                """
+            insertNoteSql = """
+                        INSERT INTO notes (transcription_id, start_time, end_time, pitch, velocity)
+                        VALUES (%s, %s, %s, %s, %s);
+                        """
+            qpm = noteSequence["tempos"][0]["qpm"]
+        
+            await curs.execute(insertTranscriptionSql, (qpm, fileName))
+            res = await curs.fetchone()
+            transcriptionId = res[0]
+            for note in noteSequence["notes"]:
+                await conn.execute(insertNoteSql, (transcriptionId, note["startTime"]*1000,
+                                                    note["endTime"]*1000, note["pitch"], note["velocity"]))
+
+    return FileResponse(path=midiFilePath, media_type='audio/midi')
 
 
--- services/midi-generator/model/transcriptions.py
+++ services/midi-generator/model/transcriptions.py
@@ -1,17 +1,53 @@
 from piano_transcription_inference import PianoTranscription, sample_rate, load_audio
-import tkinter as tk
-from tkinter import filedialog
+from beam import Image, function, Volume, CloudBucket, CloudBucketConfig
 import os
+import boto3
 
-def generateTranscription(filepath, outputPath, fileName):
 
-  # Load audio
-  (audio, _) = load_audio(filepath, sr=sample_rate, mono=True)
+async def getMidiEvents(file):
+    # Write sent audio file to S3
+    s3 = boto3.resource('s3')
+    BUCKET = "pianisto-transcriptions"
+    s3.Bucket(BUCKET).put_object(Key=file.filename, Body=await file.read())
 
-  # Transcriptor
-  checkpoint_path = os.path.join(os.path.dirname(__file__), 'CRNN_note_F1=0.9677_pedal_F1=0.9186.pth')
-  transcriptor = PianoTranscription(device='cpu', checkpoint_path=checkpoint_path)
+    # Transcribe and write out to MIDI file
+    est_note_events, est_pedal_events = _getMidiEvents.remote(file.filename)
 
-  # Transcribe and write out to MIDI file
-  transcribed_dict = transcriptor.transcribe(audio, outputPath+fileName+'.mid')
-  return transcribed_dict
+    return est_note_events, est_pedal_events
+
+mount_path = "./audio"
+s3_bucket = CloudBucket(
+    name="audio",
+    mount_path=mount_path,
+    config=CloudBucketConfig(
+        access_key="ACCESS_KEY",
+        secret_key="SECRET_KEY",
+        endpoint="https://pianisto-transcriptions.s3.us-west-2.amazonaws.com/"
+    ),
+)
+@function(
+    name="generate-transcription",
+    cpu=3,
+    memory="16Gi",
+    gpu="T4",
+    volumes=[s3_bucket],
+    image=Image(
+        python_version="python3.10",
+        python_packages=[
+            "torch",
+            "numpy",
+            "librosa==0.9.2",
+        ],
+    )
+)
+def _getMidiEvents(fileName):
+    print(os.listdir("../audio"))
+    print(os.listdir("./checkpoint"))
+    # Transcriptor
+    inputFilePath = f'../audio/{fileName}'
+    (audio, _) = load_audio(inputFilePath, sr=sample_rate, mono=True)
+
+    checkpoint_path = os.path.join(os.path.dirname(__file__), '../checkpoint/CRNN_note_F1=0.9677_pedal_F1=0.9186.pth')
+    transcriptor = PianoTranscription(device='cuda', checkpoint_path=checkpoint_path)
+    est_note_events, est_pedal_events, _ = transcriptor.getMidiEvents(audio)
+    return est_note_events, est_pedal_events
